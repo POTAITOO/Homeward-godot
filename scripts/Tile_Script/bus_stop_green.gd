c5518197e059fa5a4be8428ae120352b65bf6b81
myeonzz <balanquitjayar5@gmail.com>
2026-05-28 10:13:40 +0800

    Huge Update
    6x6 board

---
+++ scripts/Tile_Script/bus_stop_green.gd
@@ -0,0 +1,13 @@
+extends Area2D
+
+#Sheet Variable
+@onready var anim: AnimatedSprite2D = $AnimatedSprite2D
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	anim.play("default")
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
---
+++ scripts/Tile_Script/plain.gd
@@ -0,0 +1,6 @@
+extends Area2D
+
+@onready var anim: AnimatedSprite2D = $AnimatedSprite2D
+
+func _ready() -> void:
+	anim.play("default")
